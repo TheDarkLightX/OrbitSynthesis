034ce5c0a17a77fe6f44d64a3b2d9e2dba4e7020
TheDarkLightX <TheDarkLightX@users.noreply.github.com>
2026-08-12 23:22:28 -0400

    export parameter patchability oracle

--- src/orbitsynthesis/__init__.py
+++ src/orbitsynthesis/__init__.py
@@ -12,21 +12,33 @@ from .discriminator_compile import (
     selector,
 )
 from .finite_algebra import FiniteAlgebra, FiniteOperation, InternalIsomorphism
+from .patchability import (
+    ExtensionFailure,
+    PatchabilityResult,
+    first_extension_failure,
+    has_pointed_extension_property,
+    parameter_patchability_number,
+)
 from .safety import FiniteSafetyGame, SafetySolution
 
 __all__ = [
     "Const",
     "Disc",
     "Expr",
+    "ExtensionFailure",
     "FiniteAlgebra",
     "FiniteOperation",
     "FiniteSafetyGame",
     "InternalIsomorphism",
+    "PatchabilityResult",
     "SafetySolution",
     "Var",
     "compile_table",
     "compile_vector_table",
     "distinct_node_count",
     "evaluate",
+    "first_extension_failure",
+    "has_pointed_extension_property",
+    "parameter_patchability_number",
     "selector",
 ]
