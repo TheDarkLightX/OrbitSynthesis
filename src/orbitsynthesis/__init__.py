1d8b0525e508d535eb2ca1ab09c63a691da01c5d
TheDarkLightX <TheDarkLightX@users.noreply.github.com>
2026-08-12 23:13:34 -0400

    export parameterized discriminator compiler

--- src/orbitsynthesis/__init__.py
+++ src/orbitsynthesis/__init__.py
@@ -1,12 +1,32 @@
 """OrbitSynthesis standalone finite-algebra research kernel."""
 
+from .discriminator_compile import (
+    Const,
+    Disc,
+    Expr,
+    Var,
+    compile_table,
+    compile_vector_table,
+    distinct_node_count,
+    evaluate,
+    selector,
+)
 from .finite_algebra import FiniteAlgebra, FiniteOperation, InternalIsomorphism
 from .safety import FiniteSafetyGame, SafetySolution
 
 __all__ = [
+    "Const",
+    "Disc",
+    "Expr",
     "FiniteAlgebra",
     "FiniteOperation",
-    "InternalIsomorphism",
     "FiniteSafetyGame",
+    "InternalIsomorphism",
     "SafetySolution",
+    "Var",
+    "compile_table",
+    "compile_vector_table",
+    "distinct_node_count",
+    "evaluate",
+    "selector",
 ]
